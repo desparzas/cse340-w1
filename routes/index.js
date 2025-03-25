84d394f672902a0311c80ea713b518dffa9643d6
desparzas <desparzas7902@gmail.com>
2025-03-25 11:54:40 -0500

    week 03 start

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const baseController = require("../controllers/baseController");
 
 // GET home page
-router.get('/', (req, res) => {
-    res.render('index', { title: 'CSE Motors' });
-});
+router.get('/', baseController.buildHome);
 
 module.exports = router;
---
+++ utilities/index.js
@@ -0,0 +1,24 @@
+const invModel = require("../models/inventory-model");
+const Util = {};
+
+Util.getNav = async function () {
+  let data = await invModel.getClassifications();
+  let list = "<ul class = 'navbar'>";
+  list += '<li><a href="/" title="Home page">Home</a></li>';
+  data.rows.forEach((row) => {
+    list += "<li>";
+    list +=
+      '<a href="/inv/type/' +
+      row.classification_id +
+      '" title="See our inventory of ' +
+      row.classification_name +
+      ' vehicles">' +
+      row.classification_name +
+      "</a>";
+    list += "</li>";
+  });
+  list += "</ul>";
+  return list;
+};
+
+module.exports = Util;
