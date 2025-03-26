898c1cfe51a57507bb4a53907dfbc3c5fc32cd0a
desparzas <desparzas7902@gmail.com>
2025-03-25 23:53:15 -0500

    validations

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -2,11 +2,22 @@ const express = require("express");
 const router = new express.Router();
 const utilities = require("../utilities");
 const accountController = require("../controllers/accountController");
+const regValidate = require("../utilities/account-validation");
 
 // Route to deliver the login view
 router.get("/login", accountController.buildLogin);
 router.get("/register", utilities.handleErrors(accountController.buildRegister));
 router.post("/register", utilities.handleErrors(accountController.registerAccount));
+
+
+
+// Process the registration data
+router.post(
+  "/register",
+  regValidate.registrationRules(),
+  regValidate.checkRegData,
+  utilities.handleErrors(accountController.registerAccount)
+);
 
 // Error handler middleware
 router.use((err, req, res, next) => {
